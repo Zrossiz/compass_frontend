9583f4dadd09d6843e8b224e6a1ffbb3a4cb7e72
Zrossiz <soon.pluxury@mail.ru>
2026-09-21 21:23:29 +0300

    chore: add spheres list styles

--- src/modules/spheres/components/SphereCatalogItem/SphereCatalogItem.tsx
+++ src/modules/spheres/components/SphereCatalogItem/SphereCatalogItem.tsx
@@ -1,13 +1,16 @@
 import styles from './SphereCatalogItem.module.scss';
 import { SphereCatalogItemProps } from './SphereCatalogItem.props';
+import Link from 'next/link';
 
 export const SphereCatalogItem = ({ sphere }: SphereCatalogItemProps) => {
   return (
     <li className={styles.wrapper}>
-      <span>Заголовок: {sphere.title}</span>
-      <br />
-      <span>Описание: {sphere.description}</span>
-      <br />
+      <Link href={`/spheres/${sphere.id}`}>
+        <span>Заголовок: {sphere.title}</span>
+        <br />
+        <span>Описание: {sphere.description}</span>
+        <br />
+      </Link>
     </li>
   );
 };
